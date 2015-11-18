01c4ecd92655bdca4702b054a8cc292a0f06c4b3
JaZahn <jcleveng@fas.harvard.edu>
2015-11-18 14:36:11 -0500

    adding controller and directive routes

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 angular.module('directings', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider){
   $routeProvider
-    .when('/', {
-      templateUrl: 'templates/main.html'
+    .when('/controller', {
+      templateUrl: 'templates/controller.html'
+    })
+    .when('/directive', {
+      templateUrl: 'templates/directive.html'
+    })
+    .otherwise({
+      redirectTo: '/controller'
     });
 }]);
