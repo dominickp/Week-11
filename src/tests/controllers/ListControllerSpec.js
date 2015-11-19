548b728e2c8066627ba43e7b0066fe4fd949256e
Dominick Peluso <peluso.dominick@gmail.com>
2015-11-18 20:23:06 -0500

    Add controller test

--- src/tests/controllers/ListControllerSpec.js
+++ src/tests/controllers/ListControllerSpec.js
@@ -1,12 +1,31 @@
 describe("ListController", function(){
 
-  beforeEach(function(){
+    var listController, posts;
+    var mockPost = {
+        id: 1
+    };
+
+    beforeEach(function(){
     module('directings');
 
-  });
-  describe("display", function(){
-    it("should set a current post on the service", function(){
+        // Injector gets services
+        inject(function($controller, $rootScope, $injector){
+            listController = $controller('ListController', { $scope:$rootScope.$new() })
+            posts = $injector.get('posts');
+        });
 
     });
+
+
+  describe("display", function(){
+
+    it("should set a current post on the service", function(){
+        expect(posts.current).toBeUndefined();
+        listController.display(mockPost);
+        expect(posts.current).not.toBeUndefined();
+        expect(posts.current.id).toBe(mockPost.id);
+    });
+
   });
+
 });
